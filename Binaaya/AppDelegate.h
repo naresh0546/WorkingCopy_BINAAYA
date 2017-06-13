80d899c32052e1e3488f50f089012b48a8466d8c
Apple <Apple@Apples-iMac.local>
2017-06-13 10:24:05 +0530

    Initial Commit

---
+++ Binaaya/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Binaaya
+//
+//  Created by Apple on 13/06/17.
+//  Copyright © 2017 SmartMobile Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
