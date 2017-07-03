1ccbf74f5b547c9d55063b448df036585b9fe5e9
Naresh <naresh0546@gmail.com>
2017-07-03 14:44:03 +0530

    first commit

---
+++ Binaaya/SampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  SampleViewController.h
+//  Binaaya
+//
+//  Created by SmartMobile Technologies on 03/07/17.
+//  Copyright © 2017 SmartMobile Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleViewController : UIViewController
+
+@end
